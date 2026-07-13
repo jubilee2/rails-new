a3b5fa4839da10a599038f42898689b3cfcd8e84
jubilee2 <jubilee2@gmail.com>
2026-07-13 10:06:26 -0500

    Ensure demo login user exists on startup

--- rails72-azure/db/seeds.rb
+++ rails72-azure/db/seeds.rb
@@ -1,7 +1,7 @@
 email = ENV.fetch("DEMO_LOGIN_EMAIL", "admin@example.com")
 password = ENV.fetch("DEMO_LOGIN_PASSWORD", "password")
 
-User.find_or_create_by!(email:) do |user|
-  user.password = password
-  user.password_confirmation = password
-end
+user = User.find_or_initialize_by(email: email)
+user.password = password
+user.password_confirmation = password
+user.save!
